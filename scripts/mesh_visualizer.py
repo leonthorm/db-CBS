abce0c91ed188f3e42e195c40e01cec4cd1c37d2
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2024-12-22 19:59:26 +0200

    init guess and visualizer for connected unicycles

--- scripts/mesh_visualizer.py
+++ scripts/mesh_visualizer.py
@@ -15,6 +15,21 @@ import time
 # add start/goal robots with different color
 # html out file as arg
 
+DnametoColor = {
+    "red": 0xff0000,
+    "green": 0x00ff00,
+    "blue": 0x0000ff,
+    "yellow": 0xffff00,
+    "white": 0xffffff,
+    "black": 0x000000,
+    "cyan": 0x00ffff,
+    "magenta": 0xff00ff,
+    "orange": 0xffa500,
+    "purple": 0x800080
+}
+
+
+
 def visualize(env_file, result_file, video_file, payload_file=None):
     vis = meshcat.Visualizer()
     # vis.open()
@@ -28,10 +43,12 @@ def visualize(env_file, result_file, video_file, payload_file=None):
     if payload_file is not None:
       with open(payload_file, "r") as f:
         payload_states = yaml.safe_load(f)
-      pstates = np.array(payload_states["payload"])
-      draw_payload = True
-      vis["payload"].set_object(
-                g.Mesh(g.Sphere(0.01), g.MeshLambertMaterial(color="r",opacity=1.0)))
+
+      if "payload" in payload_states and payload_states["payload"] is not None:
+        pstates = np.array(payload_states["payload"])
+        draw_payload = True
+        vis["payload"].set_object(
+                  g.Mesh(g.Sphere(0.01), g.MeshLambertMaterial(color="r",opacity=1.0)))
 
     vis["/Cameras/default"].set_transform(
         tf.translation_matrix([0, 0, 0]).dot(
@@ -62,13 +79,10 @@ def visualize(env_file, result_file, video_file, payload_file=None):
       for s in result["result"][i]["states"]:
         state.append(s)
       states.append(state)
-
-      vis["Quadrotor" + str(name_robot)].set_object(g.StlMeshGeometry.from_file('../meshes/cf2_assembly.stl'), g.MeshLambertMaterial(color="green"))
-      # for the start and goal
-      # vis["Quadrotor_start" + str(name_robot)].set_object(g.StlMeshGeometry.from_file('../meshes/cf2_assembly.stl'), g.MeshLambertMaterial(color="blue"))
-      # vis["Quadrotor_start" + "0"].set_transform(tf.translation_matrix([1,0,0]).dot(
-      #         tf.quaternion_matrix(np.array([1,0,0,0]))))
-      
+      if "quad" in data["robots"][0]["type"]:
+        vis["Quadrotor" + str(name_robot)].set_object(g.StlMeshGeometry.from_file('../meshes/cf2_assembly.stl'), g.MeshLambertMaterial(color="green"))
+      elif "unicycle" in data["robots"][0]["type"]:
+        vis["unicycle" + str(name_robot)].set_object(g.Mesh(g.Box([0.1, 0.05, 0.05]), material=g.MeshLambertMaterial(color=list(DnametoColor.items())[name_robot][1])))
       name_robot+=1
 
 
@@ -81,11 +95,16 @@ def visualize(env_file, result_file, video_file, payload_file=None):
             robot_state = states[l][-1]
           else:
             robot_state = states[l][k]
-          frame["Quadrotor" + str(l)].set_transform(tf.translation_matrix(robot_state[0:3]).dot(
-              tf.quaternion_matrix(np.array([robot_state[6],robot_state[3],robot_state[4],robot_state[5]]))))
+          if "quad" in data["robots"][0]["type"]:
+
+            frame["Quadrotor" + str(l)].set_transform(tf.translation_matrix(robot_state[0:3]).dot(
+                tf.quaternion_matrix(np.array([robot_state[6],robot_state[3],robot_state[4],robot_state[5]]))))
+          elif "unicycle" in data["robots"][0]["type"]:
+            frame["unicycle" + str(l)].set_transform(tf.translation_matrix([robot_state[0], robot_state[1], 0]).dot(
+                tf.quaternion_matrix(tf.quaternion_from_euler(0,0,robot_state[2]))))
+
           if draw_payload:
             frame["payload"].set_transform(tf.translation_matrix(pstates[k,0:3]).dot(tf.quaternion_matrix([1,0,0,0])))
-      # time.sleep(0.1)
     vis.set_animation(anim)
     res = vis.static_html()
     with open(video_file, "w") as f:
