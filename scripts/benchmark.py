0f489aae24fda88e1a60ea42dd2420c97b880135
akmaralAW <moldagalieva@tu-berlin.de>
2025-02-02 18:04:35 +0100

    drones for trials=5

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -150,7 +150,7 @@ def main():
 		# "swap1_double_integrator",
 		# 2 robot cases
 		# "swap2_unicycle",
-		"swap2_unicycle_sphere",
+		# "swap2_unicycle_sphere",
 		# "swap2_unicycle_kink",
 		# "swap2_double_integrator",
 		# "swap2_trailer",
@@ -159,9 +159,9 @@ def main():
 		# "makespan_vs_soc_1",
 		# "makespan_vs_soc_0",
 		# "alcove_unicycle",
-		"alcove_unicycle_sphere",
+		# "alcove_unicycle_sphere",
 		# "at_goal_unicycle",
-		"at_goal_unicycle_sphere",
+		# "at_goal_unicycle_sphere",
 		# 3 robot cases
 		# "swap3_unicycle",
 		# "swap3_unicycle_sphere",
@@ -193,37 +193,38 @@ def main():
 		# "window4_demo",
 
 		# 3D scenarios with octomap
-		# "drone1c",
-		# "drone2c",
-		# "drone4c",
-		# "drone8c",
-		# "drone10c",
-		# "drone12c",
-		# "drone16c",
+		"drone1c",
+		"drone2c",
+		"drone4c",
+		"drone8c",
+		"drone10c",
+		"drone12c",
+		"drone16c",
 		# "drone24c",
 		# "drone28c",
 		# "drone32c",
 		# "swap3_drone",
 		# "wall_drone8c",
+		# "wall_drone10c",
 	]
 # 
 	# add random cases
-	for kind in ["unicycle_sphere", "hetero"]: 
-		for n in [2,4,8]:
-			for k in range(10):
-				instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
+	# for kind in ["unicycle_sphere", "hetero"]: 
+		# for n in [2,4,8]:
+			# for k in range(10):
+				# instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
 
 	algs = [
-		"sst",
-		"s2m2",
-		"k-cbs",
-		"db-cbs",
-		"db-ecbs",
-		# "db-ecbs-residual",
-		# "db-ecbs-conservative",
+		# "sst",
+		# "s2m2",
+		# "k-cbs",
+		# "db-cbs",
+		# "db-ecbs",
+		"db-ecbs-residual",
+		"db-ecbs-conservative",
 	]
-	trials = 10 
-	timelimit = 5*60 
+	trials = 5 
+	timelimit = 2*60*60 
 
 	tasks = []
 	for instance in instances:
@@ -256,8 +257,8 @@ def main():
 	# (Path("../results") / 'table.pdf').unlink()
 	# (Path("../results") / 'stats.pdf').unlink()
 
-	# paper_tables.write_table6(trials, timelimit)
-	paper_tables.write_table7(trials, timelimit) # 2D case
+	paper_tables.write_table6(trials, timelimit)
+	# paper_tables.write_table7(trials, timelimit) # 2D case
 
 if __name__ == '__main__':
 	main()
